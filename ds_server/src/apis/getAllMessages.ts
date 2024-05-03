40fd5218321a5bb963b3096a120475695a941d57
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.fritz.box>
2024-05-03 10:40:05 +0200

    Pusing to main whatever

--- ds_server/src/apis/getAllMessages.ts
+++ ds_server/src/apis/getAllMessages.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express'
-
 import { Message } from '../models/model.js'
 
 export const getAllMessages = async (req: Request, res: Response) => {
   console.log('ok')
   try {
     const messages = await Message.find()
-    if (!messages) {
-      res.status(404).json({ success: false, message: 'No messages found' })
+    if (!messages || messages.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'No messages found' })
     }
     console.log(messages)
-    res.status(201).json(messages)
+    return res.status(200).json(messages)
   } catch (error) {
     console.error('Error:', error)
     return res
