67b2f590093544149ddc83d9afbd30e843bf5114
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.fritz.box>
2024-05-03 10:38:42 +0200

    Pushing to main, don't even care

--- ds_client/src/apis/sendPost.ts
+++ ds_client/src/apis/sendPost.ts
@@ -4,7 +4,7 @@ interface IPostProps {
 }
 export const sendPost = async ({ postBody }: IPostProps) => {
   try {
-    await axios.post('/api/post-message', {
+    await axios.post('http://localhost:3001/api/post-message', {
       message: postBody,
     })
   } catch (error) {
--- ds_client/src/components/PostMessageForm.tsx
+++ ds_client/src/components/PostMessageForm.tsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import { Textarea } from '@/components/ui/textarea'
 import { Button } from './ui/button'
-import { getPosts } from '@/apis/getPosts'
+import { getAllMessages } from '@/apis/getAllMessages'
+import { sendPost } from '@/apis/sendPost'
+
 export const PostMessageForm = () => {
-  const [setPost, post] = useState<string>('')
+  const [post, setPost] = useState<string>('')
+
+  const handleGetMessages = async () => {
+    try {
+      // await getAllMessages()
+      await sendPost({ postBody: post })
+      console.log('Messages recieved')
+    } catch (error) {
+      console.error('Error getting messages:', error)
+    }
+  }
+
+  const handlePostChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setPost(event.target.value)
+  }
 
   return (
     <div className="flex flex-col align-center items-center gap-4">
-      <Textarea className="max-w-2xl" placeholder="Write something..." />
-      <Button onClick={getPosts}>post message</Button>
+      <Textarea
+        className="max-w-2xl"
+        placeholder="Write something..."
+        value={post}
+        onChange={handlePostChange}
+      />
+      <Button onClick={handleGetMessages} type="submit">
+        post message
+      </Button>{' '}
     </div>
   )
 }
